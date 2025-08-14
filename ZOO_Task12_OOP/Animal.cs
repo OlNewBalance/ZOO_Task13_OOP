d3b9a3d7254482b1fdda5ea7c228e07968dbd39e
Oleg <finkton23@gmail.com>
2025-08-14 20:39:15 +0500

    Update Animal.cs

--- ZOO_Task12_OOP/Animal.cs
+++ ZOO_Task12_OOP/Animal.cs
@@ -1,6 +1,15 @@
-﻿namespace Task12;
+namespace Task12;
 
 public class Animal
 {
-        
+    public string Voice;
+    public bool Sex;
+    public string SexDisplay => Sex ? "Самец" : "Самка";
+    private static readonly Random _random = new Random();
+
+    public Animal(string voice, bool sex)
+    {
+        Voice = voice;
+        Sex = sex;
+    }
 }
