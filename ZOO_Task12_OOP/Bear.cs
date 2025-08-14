9340b6e3c2d82275d4f14f3697a40ed1b62530d1
Oleg <finkton23@gmail.com>
2025-08-14 20:40:23 +0500

    Update Bear.cs

--- ZOO_Task12_OOP/Bear.cs
+++ ZOO_Task12_OOP/Bear.cs
@@ -1,6 +1,13 @@
-﻿namespace Task12;
+namespace Task12;
 
 public class Bear : Animal
 {
-        
+    static Random random = new Random();
+
+    public Bear() : base(
+        voice: "Ruaaaaaaaaarrr",
+        sex: random.Next(2) == 0
+    )
+    {
+    }
 }
