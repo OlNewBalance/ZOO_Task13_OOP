6599c7db4a509103e3108bdda84ff113a240f9d5
Oleg <finkton23@gmail.com>
2025-08-14 20:41:01 +0500

    Update Monkey.cs

--- ZOO_Task12_OOP/Monkey.cs
+++ ZOO_Task12_OOP/Monkey.cs
@@ -1,6 +1,13 @@
-﻿namespace Task12;
+namespace Task12;
 
 public class Monkey : Animal
 {
-        
+    static Random random = new Random();
+
+    public Monkey() : base(
+        voice: "AGUGUGUGUUGUAKAKKAKAKAKKAKAKhhhhhh",
+        sex: random.Next(2) == 0
+    )
+    {
+    }
 }
