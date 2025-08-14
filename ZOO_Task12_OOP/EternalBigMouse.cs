a80c7c2620503e84ceae3be5eb22b38313ee7299
Oleg <finkton23@gmail.com>
2025-08-14 20:40:40 +0500

    Update EternalBigMouse.cs

--- ZOO_Task12_OOP/EternalBigMouse.cs
+++ ZOO_Task12_OOP/EternalBigMouse.cs
@@ -1,6 +1,13 @@
-﻿namespace Task12;
+namespace Task12;
 
 public class EternalBigMouse : Animal
 {
-        
+    static Random random = new Random();
+
+    public EternalBigMouse() : base(
+        voice: "PIPIPIPIPIPIPIPIPIPIPIPIPIPIPIPIPIPIPI",
+        sex: random.Next(2) == 0
+    )
+    {
+    }
 }
